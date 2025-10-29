0057dffda1cfb4ca5998cdad3509811603f830a5
VoxNut <bnhut465@gmail.com>
2025-10-29 16:57:08 +0700

    remove .idea

---
+++ src/main/java/com/java_web/models/Student.java
@@ -0,0 +1,16 @@
+
+
+package com.java_web.models;
+public class Student {
+	
+	private String name;
+	private String id;
+	
+	
+	public Student(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	
+}
